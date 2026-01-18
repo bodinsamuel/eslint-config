982b4a16c2e540db67c8de15312a5e17721ff219
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2026-01-18 16:21:32 +0100

    better if I actually export it

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,11 @@
 import base from './configs/base.mjs';
 import strict from './configs/strict.mjs';
+import tailwind from './configs/tailwind.mjs';
 
 export default {
   configs: {
     base: base,
     strict: strict,
+    tailwind: tailwind,
   },
 };
